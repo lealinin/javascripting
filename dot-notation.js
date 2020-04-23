7a25bbcbce87e88083198e250ebf2d62f32f112c
lealinin <lea.linin@gmail.com>
2020-04-23 13:07:28 -0700

    Alert John by extracting information from the person object.

---
+++ dot-notation.js
@@ -0,0 +1,10 @@
+// Alert "John" by extracting information from the person object.
+
+const person = {
+  firstName: "John",
+  lastName: "Doe"
+};
+
+// alert(person.firstName)
+
+console.log(person.firstName);
