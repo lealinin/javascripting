f519c4397bb9153cfbdbe8fe0bbf5e59ae4320c0
lealinin <lea.linin@gmail.com>
2020-04-24 13:23:03 -0700

    Reverse a string with a for loop

---
+++ reverse-str.js
@@ -0,0 +1,12 @@
+// Reverse a String
+
+const reverseString = function(str) {
+  let newString = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i]
+  }
+  return newString;
+}
+
+console.log(reverseString("hello"));
